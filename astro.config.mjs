1384464277c5194465d257ffa1c44feed2367fcd
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2024-05-27 14:16:24 +0200

    Changed domain to kwasniokpatryk.pl

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pkwasniok.xyz",
+  site: "https://kwasniokpatryk.pl",
   output: "static",
   prefetch: {
     prefetchAll: true
